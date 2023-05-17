184004b51d448a3b395c0e3a215f773326ad7089
Do Trung Tu <tudo1611@gmail.com>
2023-05-17 11:34:35 +0700

    first commit

---
+++ src/BaiTapLayoutComponent/Item.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+export default class Item extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-3">
+                <div className="card" style={{ width: "15rem" }}>
+                  <i className="fab fa-amazon-pay text-center p-4"></i>
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-3">
+                <div className="card" style={{ width: "15rem" }}>
+                  <i className="fab fa-amazon-pay text-center p-4"></i>
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-3">
+                <div className="card" style={{ width: "15rem" }}>
+                  <i className="fab fa-amazon-pay text-center p-4"></i>
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-3">
+                <div className="card" style={{ width: "15rem" }}>
+                  <i className="fab fa-amazon-pay text-center p-4"></i>
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        ;
+      </div>
+    );
+  }
+}
